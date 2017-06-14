aa629b3d6afbfa92e908121a5a52d19ec3349a7b
Dulanjan <www.ruxian@gmail.com>
2017-06-14 23:07:19 +0530

    Add files via upload

---
+++ CashRegister.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectMars
+{
+    public partial class CashRegister : MetroFramework.Forms.MetroForm
+    {
+        Database db = new Database();
+        int a=0;
+        string[] c= new string[2];
+        string g ;
+      
+
+        public CashRegister()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            txt_date.Text = DateTime.Now.ToString();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(a + 1);
+            dataGridView1.Rows[a].Cells[0].Value = txt_item.Text;
+              
+              dataGridView1.Rows[a].Cells[2].Value = txt_qty.Text;
+
+            db.singleData("select Discription,Unit_Price from inventory where item_Code='" + txt_item.Text + "'");
+            dataGridView1.Rows[a].Cells[1].Value = db.a;
+            dataGridView1.Rows[a].Cells[3].Value = db.b;
+            double x = Convert.ToDouble(txt_qty.Text);
+            double y= Convert.ToDouble(db.b);
+            dataGridView1.Rows[a].Cells[4].Value = (x*y).ToString();
+            a = a + 1;
+            
+            txt_item.Clear();
+             txt_qty.Clear();
+
+        }
+    }
+}
---
+++ Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProjectMars
+{
+    class Database
+    {
+        private SqlConnection con;
+        private SqlCommand cmd;
+        public string a;
+       public string b;
+        public Database()
+
+        {
+            con = new SqlConnection();
+            con.ConnectionString = "Data Source=ruxian\\SQL2016ADV;Initial Catalog=POS;Integrated Security=True";
+        }
+        public void openCon()
+        {
+            con.Open();
+        }
+        public void closeCon()
+        {
+            con.Close();
+        }
+        public void singleData(string query)
+        {
+            string[] firstVariable = new string[2];
+          //  string secondVariable = string.Empty;
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(query, con)) 
+                    {
+                        con.Open();
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                a = reader[0].ToString();
+                                b = reader[1].ToString();
+
+
+                        }
+                        }
+                    cmd.Dispose();
+                    con.Close();
+                    }
+                }
+            
+            catch (Exception)
+            {
+                MessageBox.Show("ERRRO");//test!
+            }
+            
+        }
+        public DataTable getData(string query)
+        {
+            try
+            {
+                openCon();
+            }
+            catch(SqlException)
+            {
+                MessageBox.Show("Cheack database connection");//Neeed to modify!
+            }
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+            DataTable dt = new DataTable();
+             da.Fill(dt);
+            closeCon();
+            return dt;
+        }
+    }
+}
